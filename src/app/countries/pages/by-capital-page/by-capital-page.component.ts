3cefdc408bc705b8a2bb021e69a639654af22414
Carlos Alberto Torres Naira <ctorresn@emeal.nttdata.com>
2024-04-07 21:58:41 -0500

    Consumo servicio por capital

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries/countries.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './by-capital-page.component.css'
 })
 export class ByCapitalPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor( private countriesService: CountriesService ) {}
+
+  public searchByCapital( term: string ): void {
+    this.countriesService.searchByCapital( term )
+      .subscribe( countries => {
+        this.countries = countries;
+      } );
+  }
 
 }
